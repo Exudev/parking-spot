c2fdf71c4450f88b4a5811c83b8150ade9debaee
Pablo <pablo.euardo10@gmail.com>
2024-06-18 23:19:15 -0400

    new file

---
+++ src/network/response.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+
+interface ApiResponse {
+    error: string;
+    body: string;
+}
+
+type errorCode = "forbidden"| "exists"|"server-error"|"invalid-data";
+
+type SuccessResponse = (req: Request, res: Response, message: string, status?: number) => void;
+type ErrorResponse = (req: Request, res: Response, errorCode: errorCode, status?: number, errorMessage?: string) => void;
+
+export const success: SuccessResponse = (req, res, message, status = 200) => {
+    console.log(`[response meesage:] ${message}`);
+    const response: ApiResponse = {
+        error: '',
+        body: message
+    };
+    res.status(status).send(response);
+};
+
+export const error: ErrorResponse = (req, res, errorCode, status = 500, errorMessage = '') => {
+    console.error(`[response error:] ${errorMessage}`);
+    const response: ApiResponse = {
+        error: errorCode,
+        body: ''
+    };
+    res.status(status).send(response);
+};
