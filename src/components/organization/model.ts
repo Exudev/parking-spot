b8fb3fee32e4c80ad305230a16c82909d12d365c
Pablo <pablo.euardo10@gmail.com>
2024-06-18 23:19:00 -0400

    message tools

--- src/components/organization/model.ts
+++ src/components/organization/model.ts
@@ -1,18 +1,31 @@
-import { Schema, Model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
-// Schema  
-const OrganizationSchema = new Schema({
-    name: { type: String, required: true },
-    location: {
-      type: { type: String, enum: ["Point"], required: true },
-      coordinates: { type: [Number], required: true }
-    },
-    locationDelta: {
-      type: { type: String, enum: ["Point"], required: true },
-      coordinates: { type: [Number], required: true }
-    },
-    owner: { type: String, required: true },
+export interface IOrganization extends Document {
+  name: string;
+  location: {
+    type: 'Point';
+    coordinates: number[];
+  };
+  locationDelta: {
+    type: 'Point';
+    coordinates: number[];
+  };
+  owner: string;
+}
+
+const OrganizationSchema = new Schema<IOrganization>({
+  name: { type: String, required: true },
+  location: {
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: { type: [Number], required: true },
+  },
+  locationDelta: {
+    type: { type: String, enum: ['Point'], required: true },
+    coordinates: { type: [Number], required: true },
+  },
+  owner: { type: String, required: true },
 });
 
-const model = new Model('OrganizationModel', OrganizationSchema);
-module.exports = model;
+const OrganizationModel = model<IOrganization>('Organization', OrganizationSchema);
+
+export default OrganizationModel;
