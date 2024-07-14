0226151971501d6f5e545a750835cd45f23f047d
Pablo <pablo.euardo10@gmail.com>
2024-07-13 20:41:11 -0400

    user logic star

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,6 @@
 export type errorCode = "forbidden"| "exists"|"server-error"|"invalid-data";
 export type responseCode = "created"| "updated"|"fetched"|"deleted";
-
+export type userType = "user"| "admin"|"owner"|"moderator";
 interface GeoJSONPoint {
     type: "Point";
     coordinates: [number, number]; // [longitude, latitude]
@@ -11,6 +11,15 @@ export interface Organization {
     owner: string;
     location: GeoJSONPoint,
     locationDelta: GeoJSONPoint,
+  }
+
+  export interface UserDB {
+    username: string;
+    email: string;
+    name: string;
+    lastName: string;
+    userType: userType;  //change
+    password: string;
   }
 
 export type OrganizationLookup = Pick<Organization, 'name' |'location'>;
